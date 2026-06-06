665767b1a5d8172494b15dce8ae0ae8a23467e49
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-06-06 14:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const submit = document.getElementById("submit");
+const input = document.getElementsByTagName("input");
+submit.addEventListener("click", (e) =>
+{
+  e.preventDefault();
+  alert(`First Name: ${input[0].value} Last Name: ${input[1].value} Phone Number: ${input[2].value} Email ID: ${input[3].value}`);
+})
